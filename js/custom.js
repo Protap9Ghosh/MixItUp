2c78eb2cabcfceb3ee820d77b57aba2105ee1adb
Protap9Ghosh <protapg518@gmail.com>
2022-12-13 11:40:12 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+    var mixer = mixitup('.portfolio', {
+        animation : {
+            enable : true,
+            effectsIn : 'fade translateY(-100%)',
+            duration : 900,
+            easing : 'ease-in-out',
+            // effects: 'rotateY(-150deg)',
+            // perspectiveDistance: '1000px'
+        },
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
